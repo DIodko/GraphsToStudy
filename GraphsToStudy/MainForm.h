9819510732ebe2ef1e8cd756ace7f25aeda7772e
Dima <89191006232dima@gmail.com>
2022-11-29 23:26:04 +0300

    changed mainform

--- GraphsToStudy/MainForm.h
+++ GraphsToStudy/MainForm.h
@@ -41,6 +41,8 @@ namespace GraphsToStudy {
 	private: System::Windows::Forms::Label^ label1;
 	private: System::Windows::Forms::ComboBox^ comboBox2;
 	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
 	protected:
 
 
@@ -64,17 +66,19 @@ namespace GraphsToStudy {
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->comboBox2 = (gcnew System::Windows::Forms::ComboBox());
 			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
 			this->button1->Font = (gcnew System::Drawing::Font(L"Calibri", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->button1->Location = System::Drawing::Point(353, 392);
+			this->button1->Location = System::Drawing::Point(551, 531);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(151, 41);
+			this->button1->Size = System::Drawing::Size(135, 60);
 			this->button1->TabIndex = 0;
-			this->button1->Text = L"Сформировать";
+			this->button1->Text = L"Решить";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MainForm::button1_Click);
 			// 
@@ -87,7 +91,7 @@ namespace GraphsToStudy {
 				L"5", L"6", L"7", L"8", L"9", L"10", L"11", L"12",
 					L"13", L"14", L"15"
 			});
-			this->comboBox1->Location = System::Drawing::Point(214, 232);
+			this->comboBox1->Location = System::Drawing::Point(389, 42);
 			this->comboBox1->Name = L"comboBox1";
 			this->comboBox1->Size = System::Drawing::Size(58, 31);
 			this->comboBox1->TabIndex = 2;
@@ -98,7 +102,7 @@ namespace GraphsToStudy {
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Calibri", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label1->Location = System::Drawing::Point(89, 163);
+			this->label1->Location = System::Drawing::Point(40, 42);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(332, 23);
 			this->label1->TabIndex = 3;
@@ -110,7 +114,7 @@ namespace GraphsToStudy {
 				static_cast<System::Byte>(204)));
 			this->comboBox2->FormattingEnabled = true;
 			this->comboBox2->Items->AddRange(gcnew cli::array< System::Object^  >(2) { L"Алгоритм Дейкстры", L"Алгоритм Демукрона" });
-			this->comboBox2->Location = System::Drawing::Point(520, 232);
+			this->comboBox2->Location = System::Drawing::Point(389, 95);
 			this->comboBox2->Name = L"comboBox2";
 			this->comboBox2->Size = System::Drawing::Size(192, 31);
 			this->comboBox2->TabIndex = 4;
@@ -121,17 +125,41 @@ namespace GraphsToStudy {
 			this->label2->AutoSize = true;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Calibri", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label2->Location = System::Drawing::Point(523, 163);
+			this->label2->Location = System::Drawing::Point(40, 95);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(189, 23);
 			this->label2->TabIndex = 5;
 			this->label2->Text = L"Выберите тип задания";
+			// 
+			// button2
+			// 
+			this->button2->Font = (gcnew System::Drawing::Font(L"Calibri", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button2->Location = System::Drawing::Point(133, 531);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(135, 60);
+			this->button2->TabIndex = 6;
+			this->button2->Text = L"Показать без решения";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Font = (gcnew System::Drawing::Font(L"Calibri", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button3->Location = System::Drawing::Point(340, 531);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(135, 60);
+			this->button3->TabIndex = 7;
+			this->button3->Text = L"Показать с решением";
+			this->button3->UseVisualStyleBackColor = true;
 			// 
 			// MainForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(854, 655);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->comboBox2);
 			this->Controls->Add(this->label1);
